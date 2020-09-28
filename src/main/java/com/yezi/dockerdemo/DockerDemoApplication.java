d7734151ec0066cbcf3bfc245791822694dbd334
yehounan <335376584@qq.com>
2020-09-28 17:14:36 +0800

    add Dockerfile

--- src/main/java/com/yezi/dockerdemo/DockerDemoApplication.java
+++ src/main/java/com/yezi/dockerdemo/DockerDemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @SpringBootApplication
 public class DockerDemoApplication {
@@ -15,7 +17,8 @@ public class DockerDemoApplication {
 
 
     @GetMapping("/home")
-    public String home(){
+    public String home() {
+        System.out.println("------------------------------" + LocalDateTime.now());
         return "docker demo";
     }
 }
